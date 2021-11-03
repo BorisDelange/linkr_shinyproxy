42abcf759ee97beb1964fa530e69f903798dd1f3
Boris Delange <boris.delange@gmail.com>
2021-11-03 23:56:59 +0100

    Change of directory for local database

--- R/fct_settings_app_database.R
+++ R/fct_settings_app_database.R
@@ -127,9 +127,7 @@ db_create_tables <- function(db){
 get_local_db <- function(){
   
   # Connect to local database
-  setwd(path.expand('~'))
-  db <- DBI::dbConnect(RSQLite::SQLite(), "cdwtools")
-  setwd(find.package("cdwtools"))
+  db <- DBI::dbConnect(RSQLite::SQLite(), paste0(path.expand('~'), "/cdwtools"))
   
   db_create_tables(db)
   
