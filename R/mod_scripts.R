42dd618b8ff70d736b263d1b461cc9723d129aeb
Boris Delange <boris.delange@gmail.com>
2023-06-20 22:58:53 +0200

    Remove r access from scripts page

--- R/mod_scripts.R
+++ R/mod_scripts.R
@@ -1325,7 +1325,7 @@ mod_scripts_server <- function(id = character(), r = shiny::reactiveValues(), d 
       # Variables to hide
       new_env_vars <- list("r" = NA)
       # Variables to keep
-      for (var in c("d", "m", "r", "output", "i18n")) new_env_vars[[var]] <- eval(parse(text = var))
+      for (var in c("d", "m", "output", "i18n")) new_env_vars[[var]] <- eval(parse(text = var))
       new_env <- rlang::new_environment(data = new_env_vars, parent = pryr::where("r"))
       
       options('cli.num_colors' = 1)
